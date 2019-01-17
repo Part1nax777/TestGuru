efb4992193a2d0df9aa43bf98b9cc2c830632065
Part1nax777 <serewka-ru@yandex.ru>
2019-01-17 21:26:26 +0300

    decision task_5

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,4 +1,6 @@
 class Test < ApplicationRecord
+  has_many :results
+  has_many :users, through :results
   belongs_to :category
   def self.sort_body_test_desc(title)
     joins('JOIN categories ON test.category_id = categories.id').where(categories: { title: :category })
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord(level)
+  has_many :results
+  has_many :tests, through :results
   Test.joins('JOIN results ON results.test_id = tests.id')
     .where(results: { user_id: id }, complexity: level)
 end
