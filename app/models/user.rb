a9004f49a4a41293703d1b3bcd0690b0bdf6d7bd
Рычков Сергей <s.rychkov@domain.local>
2019-01-22 10:33:10 +0300

    corrections for task_5

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   has_many :results, dependent: :destroy
   has_many :tests, through: :results, dependent: :destroy
-  has_many :user_to_tests, class_name: 'Test', foreign_key: 'author_id'
+  has_many :authored_tests, class_name: 'Test', foreign_key: 'author_id', dependent: :destroy
   def select_tests_by_level(level)
     test.where(complexity: level)
   end
--- db/seeds.rb
+++ db/seeds.rb
@@ -17,32 +17,32 @@ users = User.create!([
 ])
 
 tests = Test.create!([
-  { title: 'Математика', complexity: 3, category_id: categories[0].id, author_id: users[0].id },
-  { title: 'История', complexity: 2, category_id: categories[0].id, author_id: users[0].id },
-  { title: 'Право', complexity: 3, category_id: categories[1].id, author_id: users[1].id },
-  { title: 'Экономика', complexity: 1, category_id: categories[1].id, author_id: users[1].id },
-  { title: 'География', complexity: 1, category_id: categories[0].id, author_id: users[2].id },
-  { title: 'Программирование', complexity: 3, category_id: categories[1].id, author_id: users[2].id }
+  { title: 'Математика', complexity: 3, category: categories[0], author: users[0] },
+  { title: 'История', complexity: 2, category: categories[0], author: users[0] },
+  { title: 'Право', complexity: 3, category: categories[1], author: users[1] },
+  { title: 'Экономика', complexity: 1, category: categories[1], author: users[1] },
+  { title: 'География', complexity: 1, category: categories[0], author: users[2] },
+  { title: 'Программирование', complexity: 3, category: categories[1], author: users[2] }
 ])
 
 questions = Question.create!([
-  { body: 'Log(100)', test_id: tests[0].id },
-  { body: 'В каком году была Куликовская битва', test_id: tests[1].id },
-  { body: 'В каком году родился Петр 1', test_id: tests[1].id },
-  { body: 'Правовые отношения при наследстве', test_id: tests[2].id },
-  { body: 'Основной капиталл это', test_id: tests[3].id },
-  { body: 'Столица Чехии', test_id: tests[4].id },
-  { body: 'Язык низкого уровня', test_id: tests[5].id }
+  { body: 'Log(100)', test: tests[0] },
+  { body: 'В каком году была Куликовская битва', test: tests[1] },
+  { body: 'В каком году родился Петр 1', test: tests[1] },
+  { body: 'Правовые отношения при наследстве', test: tests[2] },
+  { body: 'Основной капиталл это', test: tests[3] },
+  { body: 'Столица Чехии', test: tests[4] },
+  { body: 'Язык низкого уровня', test: tests[5] }
 ])
 
 answers = Answer.create!([
-  { body: '2', question_id: questions[0].id },
-  { body: '1380', question_id: questions[1].id },
-  { body: '1672', question_id: questions[1].id },
-  { body: 'Семейные', question_id: questions[2].id },
-  { body: 'Машины и механизмы', question_id: questions[3].id },
-  { body: 'Прага', question_id: questions[4].id },
-  { body: 'Assembler', question_id: questions[0].id }
+  { body: '2', question: questions[0] },
+  { body: '1380', question: questions[1] },
+  { body: '1672', question: questions[1] },
+  { body: 'Семейные', question: questions[2] },
+  { body: 'Машины и механизмы', question: questions[3] },
+  { body: 'Прага', question: questions[4] },
+  { body: 'Assembler', question: questions[0] }
 ])
 
 
