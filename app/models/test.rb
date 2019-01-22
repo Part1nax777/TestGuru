dbc5266d91bc94c90c4abc528421925a8555643e
Part1nax777 <serewka-ru@yandex.ru>
2019-01-22 19:59:01 +0300

    fix correct task_5

--- app/models/test.rb
+++ app/models/test.rb
@@ -3,9 +3,9 @@ class Test < ApplicationRecord
   belongs_to :author, class_name: 'User'
   has_many :questions, dependent: :destroy
   has_many :results, dependent: :destroy
-  has_many :users, through: :results, dependent: :destroy
-  def self.sort_body_test_desc(title)
-    joins(:category).where(categories: { title: :title })
-      .order(title: :desc).pluck(:title)
-  end
+  has_many :users, through: :results
+    def self.sort_body_test_desc(title)
+      joins(:category).where(categories: { title: :title })
+        .order(title: :desc).pluck(:title)
+    end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   has_many :results, dependent: :destroy
   has_many :tests, through: :results, dependent: :destroy
   has_many :authored_tests, class_name: 'Test', foreign_key: 'author_id', dependent: :destroy
-  def select_tests_by_level(level)
-    test.where(complexity: level)
-  end
+    def tests_by_level(level)
+      test.where(complexity: level)
+    end
 end
