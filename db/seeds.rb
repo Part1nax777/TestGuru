b72059647a35bdc8bb5571770e93955906be1cb7
Part1nax777 <serewka-ru@yandex.ru>
2019-02-11 20:06:21 +0300

    fix for Task 10

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,9 +11,9 @@ categories = Category.create!([
 ])
 
 users = User.create!([
-  { name: 'Алеша Попович', email: 'ap@email.com' },
-  { name: 'Барон Мюнхаузен', email: 'bm@email.com' },
-  { name: 'Винни Пух', email: 'vp@email.com' }
+  { name: 'Алеша Попович', email: 'ap@email.com', password: '123' },
+  { name: 'Барон Мюнхаузен', email: 'bm@email.com', password: '123' },
+  { name: 'Винни Пух', email: 'vp@email.com', password: '123' }
 ])
 
 tests = Test.create!([
