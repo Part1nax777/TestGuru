896baf43914569dff45e2ea5d74150af2a2ed30a
Part1nax777 <serewka-ru@yandex.ru>
2019-02-13 23:32:19 +0300

    decision_light Task_11

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,23 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
-  helper_method :current_user,
-                :logged_in?
-
-  private
-
-  def authenticate_user!
-    unless current_user
-      cookies[:target_path] = request.fullpath
-      redirect_to login_path, alert: 'Введите логин и пароль'
+  def after_sign_in_path_for(user)
+    if user.is_a?(Admin)
+      admin_tests_path
+    else
+      super
     end
-  end
-
-  def current_user
-    @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
-  end
-
-  def logged_in?
-    current_user.present?
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,16 +1,16 @@
 class User < ApplicationRecord
-  VALID_EMAIL = /.+@.+\..+/i
+
+  devise :database_authenticatable,
+         :registerable,
+         :recoverable,
+         :rememberable,
+         :validatable,
+         :trackable,
+         :confirmable
 
   has_many :test_passages, dependent: :destroy
   has_many :tests, through: :test_passages, dependent: :destroy
   has_many :authored_tests, class_name: 'Test', foreign_key: 'author_id', dependent: :destroy
-
-  validates :name, presence: true
-  validates :email, presence: true,
-                    format: VALID_EMAIL,
-                    uniqueness: true
-
-  has_secure_password
 
   def test_passage(test)
     test_passages.order(id: :desc).find_by(test: test)
