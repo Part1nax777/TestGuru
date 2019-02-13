ac8e99fa178cad50e11e0570eda29b9c0ba184bf
Part1nax777 <serewka-ru@yandex.ru>
2019-02-14 02:03:44 +0300

    add flash for Task 11

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,8 +3,10 @@ class ApplicationController < ActionController::Base
 
   def after_sign_in_path_for(user)
     if user.is_a?(Admin)
+      flash[:notice] = "Привет #{current_user.first_name}"
       admin_tests_path
     else
+      flash[:notice] = "Привет #{current_user.first_name}"
       super
     end
   end
