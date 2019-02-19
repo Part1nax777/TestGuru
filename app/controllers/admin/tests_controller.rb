c34103db61f69f3983b3eb93cbc5a2035fc6f279
Part1nax777 <serewka-ru@yandex.ru>
2019-02-20 00:08:39 +0300

    update Task_12

--- app/controllers/admin/tests_controller.rb
+++ app/controllers/admin/tests_controller.rb
@@ -44,7 +44,7 @@ class Admin::TestsController < Admin::BaseController
   end
 
   def rescue_with_question_not_found
-    render plain: 'вопрос не найден'
+    render plain: t('.error')
   end
 
   def set_test
