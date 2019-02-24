8ce596dc45e44ae25554d9d4e57747f40b23ba8f
Part1nax777 <serewka-ru@yandex.ru>
2019-02-24 21:24:03 +0300

    decision for task 13

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,7 +7,13 @@ module ApplicationHelper
     "http://github.com/#{author}/#{repo}"
   end
 
-  def flash_message(type_flash)
-    content_tag :p, flash[type_flash], class: "flash #{type_flash}" if flash[type_flash]
+  def flash_class(level)
+    case level
+      when 'notice' then "alert alert-info"
+      when 'success' then "alert alert-success"
+      when 'error' then "alert alert-danger"
+      when 'alert' then "alert alert-warning"
+    end
   end
+
 end
