1a9b4343828f85c862c4f2974e7b90d0216f1a73
Part1nax777 <serewka-ru@yandex.ru>
2019-02-26 23:23:03 +0300

    not even items in Task 14

--- Gemfile
+++ Gemfile
@@ -40,6 +40,10 @@ gem 'bootsnap', '>= 1.1.0', require: false
 gem 'jquery-rails'
 gem 'bootstrap', '~> 4.0.0.beta'
 
+#API
+gem "octokit", "~> 4.0"
+gem 'dotenv-rails', groups: [:development, :test]
+
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
---
+++ app/services/gist_question_service.rb
@@ -0,0 +1,36 @@
+class GistQuestionService
+
+  def initialize(question, client = octokit_client)
+    @question = question
+    @test = @question.test
+    @client = client
+  end
+
+  def call
+    @client.create_gist(gist_params)
+  end
+
+  private
+
+  def gist_params
+    {
+      description: I18n.t('services.gist_question_service.description_message', title: @test.title),
+      files: {
+        'test-guru-question.txt' => {
+          content: gist_content
+        }
+      }
+    }
+  end
+
+  def gist_content
+    content = [@question.body]
+    content += @question.answers.pluck(:body)
+    content.join("\n")
+  end
+
+  def octokit_client
+    client = Octokit::Client.new(access_token: ENV['SECRET_KEY'])
+  end
+
+end
