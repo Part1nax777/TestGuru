c8e37c6c474741af9b1bfa986d1c02f5071e851b
Part1nax777 <serewka-ru@yandex.ru>
2019-03-15 21:06:04 +0300

    change host

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,7 +64,7 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "TestGuru_#{Rails.env}"
 
   config.action_mailer.perform_caching = false
-  config.action_mailer.default_url_options = { host: 'testguru-rsa.herokuapp.com' }
+  config.action_mailer.default_url_options = { host: 'polar-inlet-89363.herokuapp.com' }
   config.action_mailer.dekivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address:              'smtp.gmail.com',
