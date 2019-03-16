3811f9cc2b039c65c7c712d11a55cac33c9f6afb
Part1nax777 <serewka-ru@yandex.ru>
2019-03-16 20:51:39 +0300

    add .env

--- app/controllers/user/sessions_controller.rb
+++ app/controllers/user/sessions_controller.rb
@@ -1,6 +1,8 @@
 class User::SessionsController < Devise::SessionsController
+  before_action :authenticate_user!
+
   def create
     super
-    flash[:notice] = "Привет, #{current_user.first_name}"
+    flash[:notice] = "Привет #{current_user.first_name}"
   end
 end
